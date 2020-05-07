5db360ebff9ff03b29dfcef61df58e1c6e95819f
Charlin Agramonte <char0394@gmail.com>
2020-05-07 10:14:57 -0400

    Creating a new project with Prism

---
+++ TodoList/TodoList/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace TodoList
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+           // NavigationService.NavigateAsync(new System.Uri("", System.UriKind.Absolute));
+
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ TodoList/TodoList/Constants/NavConstants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace TodoList.Constants
+{
+    public static class NavConstants
+    {
+       
+    }
+}
